3f6a6cd7dc7c7e27280864a9c7ac20ef3b38a9f6
Strom <stromkuo@gmail.com>
2024-11-25 17:54:01 +0800

    refactor!: remove streamName parameter from CreateClient API
    BREAKING CHANGE: The CreateClient interface no longer requires the `streamName` parameter.

--- Runtime/UniKinesisClient.cs
+++ Runtime/UniKinesisClient.cs
@@ -2,7 +2,7 @@ namespace UniKinesisSDK
 {
     public static class UniKinesisClient
     {
-        public static IKinesisClient CreateClient(string kinesisPoolId, string region, string streamName)
+        public static IKinesisClient CreateClient(string kinesisPoolId, string region)
         {
 #if UNITY_WEBGL && !UNITY_EDITOR
             return new JSKinesisClient(kinesisPoolId, region);
